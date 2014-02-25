797857e20593ad336e1fd23f962e18d155ca335d
tschuy <evantschuy@gmail.com>
2014-02-25 10:31:45 -0800

    Add usage data

--- inventory.sh
+++ inventory.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 # Written by Evan Tschuy
-# v0.9.0
+# v0.9.5
 # tschuye@onid.oregonstate.edu
-# 28 January 2014 
+# 25 February 2014 
 
-# Inventory.dat location
+# data files location
 INVENTORY=/home/tschuy/Downloads/inventory.dat
+USAGE=/home/tschuy/Downloads/.usage.dat
 
 # Inventory loading function
 # Initializes arrays and loads columns from inventory.dat into arrays
 function load_inventory {
+	date +"%c" >> $USAGE	
+	echo " Inventory reloaded" >> $USAGE
 	OUTPUT=()
 	LOCATION=()
 	PRICE=()
+	PLU=()
 
-	while IFS=, read col1 col2 col3
+	while IFS=, read col1 col2 col3 col4
 	do
 	        OUTPUT+=($col1)
 	        LOCATION+=($col2)
 	        PRICE+=($col3)
+			PLU+=($col4)
 	done < $INVENTORY
 } 
 
@@ -34,9 +39,14 @@ function add_to_inventory {
 	printf "Enter item price: "
 	read NEW_PRICE
 	NEW_PRICE=${NEW_PRICE// /_}
+	printf "Enter item PLU: "
+	read NEW_PLU
+	NEW_PLU=${NEW_PLU// /_}
 
 	# Write new row to inventory.dat and reload inventory
-	echo "$NEW_NAME,$NEW_LOCATION,$NEW_PRICE" >> $INVENTORY
+	echo "$NEW_NAME,$NEW_LOCATION,$NEW_PRICE,$NEW_PLU" >> $INVENTORY
+	date +"%c" >> $USAGE
+	echo " User added new inventory item \"$NEW_NAME\"" >> $USAGE
 	printf "\nItem added!"
 	load_inventory
 }
@@ -47,11 +57,11 @@ function main {
 		clear
 
 		printf "   ___  ____  _   _   ___ _____ _____ _____   ____  _                 \n  / _ \/ ___|| | | | |_ _| ____| ____| ____| / ___|| |_ ___  _ __ ___ \n | | | \___ \| | | |  | ||  _| |  _| |  _|   \___ \| __/ _ \| '__/ _ \ \n | |_| |___) | |_| |  | || |___| |___| |___   ___) | || (_) | | |  __/\n  \___/|____/ \___/  |___|_____|_____|_____| |____/ \__\___/|_|  \___|"                                                                      
-		printf "\n\nWelcome to the unofficial OSU IEEE Store search program.\n\nOptions:\n   Enter Q to quit\n   Enter A to add to database\n   Enter R to refresh database\n\nOr, to search, enter partial part name: "
+		printf "\n\nWelcome to the unofficial OSU IEEE Store search program.\nFound an item listed as unknown? Need to report a bug?\nEmail the author! tschuye@onid.oregonstate.edu\n\nOptions:\n   Enter Q to quit\n   Enter A to add to database\n   Enter R to refresh database\n\nOr, to search, enter partial part name: "
 		read PART
 		PART=${PART// /_}
 
-		test "$PART" == "Q" && printf "\nGood bye!\n\n" && clear && break
+		test "$PART" == "Q" && date +"%c" >> $USAGE && echo " User quit" >> $USAGE && printf "\nGood bye!\n\n" && clear && break
 		test "$PART" == "A" && add_to_inventory
 		test "$PART" == "R" && load_inventory && printf "\nInventory reloaded.\n\n"
 		printf "\n"
@@ -63,15 +73,16 @@ function main {
 					ITEM_NAME=${OUTPUT[$item]//_/ } # Replace underscores with spaces (slightly hacky)
 					ITEM_LOCATION=${LOCATION[$item]//_/ } # Same here
 					ITEM_PRICE=${PRICE[$item]//_/ } # And here
-					printf "%s: %s @ %s\n" "$ITEM_NAME" "$ITEM_LOCATION" "$ITEM_PRICE"
+					ITEM_PLU=${PLU[$item]//_/ } # lastly here
+					printf "%s (PLU %s): %s @ %s\n" "$ITEM_NAME" "$ITEM_PLU" "$ITEM_LOCATION" "$ITEM_PRICE"
 					COUNTER=1
 				fi
 			done
 			PART_CLEAN=${PART//_/ }	
 			test "$COUNTER" == "0" && printf "No results found for %s" "$PART_CLEAN"
-	
 			printf "\n"
 		fi
+		date +"%c" >> $USAGE && printf " User searched for %s""$PART_CLEAN" >> $USAGE && echo >> $USAGE
 
 		printf "\n"
 		read -p "Press [Enter] key to continue..."
